ad0c0926416560ddc46389f11708b7e272809f4b
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-06-16 00:45:07 +0530

    Update test_function.h

--- test_function.h
+++ test_function.h
@@ -3,3 +3,4 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);  
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void print_reference_manual(void);
